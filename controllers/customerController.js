08212532097f3f13927500f47f27726b23c7c4b2
Joe Newton <josephnewton861@github.com>
2022-07-25 21:11:00 +0100

    adjusted controller need to figure out how to make the then block work.

--- controllers/customerController.js
+++ controllers/customerController.js
@@ -6,19 +6,24 @@ const {
 } = require('../models/customer');
 
 exports.isCustomerLoggedIn = (req, res, next) => {
-    const {username, password} = req.params;
-    fetchCustomerIfLoggedIn(username, password)
-    .then((res) => {
+    const {username, password, email} = req.params;
+    fetchCustomerIfLoggedIn(username, password, email)
+    .then((customer) => {
         res.status(200).send({customer})
-    }).catch(next)
+    }).catch(({msg}) => {
+        res.status.send(msg);
+    })
 }
 
 exports.addCustomer = (req, res, next) => {
-    const {username, password, address, email, address, postcode} = req.params;
+    const {username, password, address, email, postcode} = req.params;
     insertCustomer(username, password, address, email, address, postcode)
     .then((res) => {
         res.status(200).send({customer})
-    }).catch(next)
+    }).catch(({msg}) => {
+        console.log('back in response')
+        res.status(404).send(msg)
+    })
 }
 
 exports.removeCustomer = (req, res, next) => {
