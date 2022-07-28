892cd2c97b425ba33eb1154913a6d3c9a50553ee
Joe Newton <josephnewton861@github.com>
2022-07-28 17:03:31 +0100

    Adjusted connection file

--- db/connection.js
+++ db/connection.js
@@ -2,16 +2,24 @@ const mysql = require('mysql');
 const dotenv = require('dotenv');
 dotenv.config();
 
-const con = mysql.createConnection({
-    host: process.env.DB_HOSTNAME,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB,
-    port: process.env.DB_PORT,
-    multipleStatements: true
-});
+let con;
+let host;
+
+if (process.env.CLEARDB_DATABASE_URL) {
+    con = process.env.CLEARDB_DATABASE_URL;
+    host = process.env.DB_HOST_PROD;
+} else {
+    con = mysql.createConnection({
+        host: process.env.DB_HOSTNAME,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB,
+        port: process.env.DB_PORT,
+        multipleStatements: true
+    });
+    host = process.env.DB_HOST
+}
 const testPassword = process.env.TEST_PASSWORD
 
-const host = process.env.DB_HOST
 
 module.exports = {con, host, testPassword};
