998d2e928aada498ca82faf3c42358e4bd739b52
Joe Newton <josephnewton861@github.com>
2022-07-28 20:20:18 +0100

    test

--- app.js
+++ app.js
@@ -6,4 +6,8 @@ app.use(express.json());
 
 app.use("/api", apiRouter);
 
+const port = process.env.PORT || 3000;
+
+app.listen(port);
+
 module.exports = app;
--- listen.js
+++ listen.js
@@ -1,4 +1,4 @@
-const app = require("./app");
-const port = process.env.PORT || 3000;
+// const app = require("./app");
+// const port = process.env.PORT || 3000;
 
-app.listen(port);
+// app.listen(port);
