8d07673a7516c3e47f0f9f90e185bd552b59bc3c
Joe Newton <josephnewton861@github.com>
2022-07-28 18:12:30 +0100

    test

--- db/connection.js
+++ db/connection.js
@@ -7,22 +7,21 @@ let host;
 
 console.log('in connection file')
 
-if (process.env.CLEARDB_DATABASE_URL) {
-    con = process.env.CLEARDB_DATABASE_URL;
-    host = process.env.DB_HOST_PROD;
-    console.log('hereeeee');
-} 
-else {
-    con = mysql.createConnection({
-        host: process.env.DB_HOSTNAME,
-        user: process.env.DB_USER,
-        password: process.env.DB_PASSWORD,
-        database: process.env.DB,
-        port: process.env.DB_PORT,
-        multipleStatements: true
-    });
-    host = process.env.DB_HOST
-}
+//if (process.env.CLEARDB_DATABASE_URL) {
+    const con = process.env.CLEARDB_DATABASE_URL;
+    const host = process.env.DB_HOST_PROD;
+//} 
+// else {
+//     con = mysql.createConnection({
+//         host: process.env.DB_HOSTNAME,
+//         user: process.env.DB_USER,
+//         password: process.env.DB_PASSWORD,
+//         database: process.env.DB,
+//         port: process.env.DB_PORT,
+//         multipleStatements: true
+//     });
+//     host = process.env.DB_HOST
+// }
 const testPassword = process.env.TEST_PASSWORD
 
 
