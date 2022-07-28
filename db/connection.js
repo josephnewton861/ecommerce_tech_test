03cf29fac0735f9ba18411b164d21fd3aa0287c0
Joe Newton <josephnewton861@github.com>
2022-07-28 20:03:07 +0100

    create pool method

--- db/connection.js
+++ db/connection.js
@@ -2,13 +2,14 @@ const mysql = require('mysql');
 const dotenv = require('dotenv');
 dotenv.config();
 
-const con = mysql.createConnection({
+const con = mysql.createPool({
         host: process.env.DB_HOSTNAME,
         user: process.env.DB_USER,
         password: process.env.DB_PASSWORD,
         database: process.env.DB_HOST,
         port: process.env.DB_PORT,
     });
+
 
 
 
