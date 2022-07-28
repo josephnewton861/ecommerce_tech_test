fcf18852a113bd1d79890919b280bc3fe8e26e1c
Joe Newton <josephnewton861@github.com>
2022-07-28 20:50:18 +0100

    test

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const apiRouter = require("./routes/api");
+const {con, host} = require('../db/connection');
 
 app.use(express.json());
 
@@ -9,7 +10,23 @@ app.use("/api", apiRouter);
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-    res.send('hello world')
+    let testQuery = `SELECT * FROM ${host}.PRODUCTS ORDER BY release_date desc;`
+    return new Promise((resolve, reject) => {
+        con.query(
+            testQuery, (err, result)  => {
+                if (err) {
+                    return reject({
+                        status: 500,
+                        msg: `Unable to get products`
+                    })
+                }
+                console.log(result, 'here')
+                return resolve({products: result});
+            }
+        )
+    }).then((products) => {
+        res.status.send(products);
+    })
   })
 
 app.listen(port);
