d6130455f1758e33b6e10e1337e52dcc9409b024
Joe Newton <josephnewton861@github.com>
2022-07-28 14:57:09 +0100

    changed listen js file

--- listen.js
+++ listen.js
@@ -1,4 +1,4 @@
-const app = require("./app");
-const { PORT = 3000 } = process.env;
-
-app.listen(PORT);
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+});
