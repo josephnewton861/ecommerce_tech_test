5c5f2341ea4e8cc89d2cfb56143e30d52cb4a1d1
Joe Newton <josephnewton861@github.com>
2022-07-29 15:17:51 +0100

    test

--- db/connection.js
+++ db/connection.js
@@ -10,15 +10,15 @@ dotenv.config();
     //  process.env.DB_PROD_HOSTNAME, process.env.DB_PROD_USER, process.env.DB_PROD_PASSWORD, process.env.DB_PROD_HOST, 
     //  process.env.DB_PROD_PORT);
     const host = process.env.DB_HOST_PROD;
-    // con = mysql.createConnection({
-    //     host: process.env.DB_PROD_HOSTNAME,
-    //     user: process.env.DB_PROD_USER,
-    //     password: process.env.DB_PROD_PASSWORD,
-    //     database: process.env.DB_PROD_HOST,
-    //     port: process.env.DB_PROD_PORT,
-    //     multipleStatements: true
-    // });
-   const con = mysql.createConnection(process.env.CLEARDB_DATABASE_URL)
+    const con = mysql.createConnection({
+        host: process.env.DB_PROD_HOSTNAME,
+        user: process.env.DB_PROD_USER,
+        password: process.env.DB_PROD_PASSWORD,
+        database: process.env.DB_PROD_HOST,
+        port: process.env.DB_PROD_PORT,
+        multipleStatements: true
+    });
+   //const con = mysql.createConnection(process.env.CLEARDB_DATABASE_URL)
 //    con.connect();
 //} 
 // else {
