c199e9f189cee22500a37c0f8b28e107f0eeb072
Joe Newton <josephnewton861@github.com>
2022-07-29 15:10:24 +0100

    test.

--- db/connection.js
+++ db/connection.js
@@ -2,14 +2,14 @@ const mysql = require('mysql');
 const dotenv = require('dotenv');
 dotenv.config();
 
-let con;
-let host;
+// let con;
+// let host;
 
-if(process.env.CLEARDB_DATABASE_URL) {
-    console.log('here',
-     process.env.DB_PROD_HOSTNAME, process.env.DB_PROD_USER, process.env.DB_PROD_PASSWORD, process.env.DB_PROD_HOST, 
-     process.env.DB_PROD_PORT);
-    host = process.env.DB_HOST_PROD;
+// if(process.env.CLEARDB_DATABASE_URL) {
+    //console.log('here',
+    //  process.env.DB_PROD_HOSTNAME, process.env.DB_PROD_USER, process.env.DB_PROD_PASSWORD, process.env.DB_PROD_HOST, 
+    //  process.env.DB_PROD_PORT);
+    const host = process.env.DB_HOST_PROD;
     // con = mysql.createConnection({
     //     host: process.env.DB_PROD_HOSTNAME,
     //     user: process.env.DB_PROD_USER,
@@ -18,19 +18,20 @@ if(process.env.CLEARDB_DATABASE_URL) {
     //     port: process.env.DB_PROD_PORT,
     //     multipleStatements: true
     // });
-   con = mysql.createConnection(process.env.CLEARDB_DATABASE_URL)
+   const con = mysql.createConnection(process.env.CLEARDB_DATABASE_URL)
 //    con.connect();
-} else {
-    con = mysql.createConnection({
-        host: process.env.DB_HOSTNAME,
-        user: process.env.DB_USER,
-        password: process.env.DB_PASSWORD,
-        database: process.env.DB,
-        port: process.env.DB_PORT,
-        multipleStatements: true
-    });
-    host = process.env.DB_HOST
-}
+//} 
+// else {
+//     con = mysql.createConnection({
+//         host: process.env.DB_HOSTNAME,
+//         user: process.env.DB_USER,
+//         password: process.env.DB_PASSWORD,
+//         database: process.env.DB,
+//         port: process.env.DB_PORT,
+//         multipleStatements: true
+//     });
+//     host = process.env.DB_HOST
+// }
 const testPassword = process.env.TEST_PASSWORD
 
 
