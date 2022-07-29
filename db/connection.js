b039c74ffdddc7dc0387d0d2b66aa136ff12f32f
Joe Newton <josephnewton861@github.com>
2022-07-29 15:59:25 +0100

    test

--- db/connection.js
+++ db/connection.js
@@ -5,21 +5,21 @@ dotenv.config();
 
 // let host;
 const host = process.env.DB_PROD_HOST
-console.log('here',
- process.env.DB_PROD_HOSTNAME, process.env.DB_PROD_USER, process.env.DB_PROD_PASSWORD, process.env.DB_PROD_HOST, 
- process.env.DB_PROD_PORT);
+// console.log('here',
+//  process.env.DB_PROD_HOSTNAME, process.env.DB_PROD_USER, process.env.DB_PROD_PASSWORD, process.env.DB_PROD_HOST, 
+//  process.env.DB_PROD_PORT);
 // if(process.env.CLEARDB_DATABASE_URL) {
 //     const host = process.env.DB_HOST_PROD;
 //     console.log('IN CLEAR DB')
-   const con = mysql.createPool({
-        host: process.env.DB_PROD_HOSTNAME,
-        user: process.env.DB_PROD_USER,
-        password: process.env.DB_PROD_PASSWORD,
-        database: process.env.DB_PROD_HOST,
-        port: process.env.DB_PROD_PORT,
-        multipleStatements: true
-    });
-   //const con = mysql.createPool(process.env.CLEARDB_DATABASE_URL)
+//    const con = mysql.createPool({
+//         host: process.env.DB_PROD_HOSTNAME,
+//         user: process.env.DB_PROD_USER,
+//         password: process.env.DB_PROD_PASSWORD,
+//         database: process.env.DB_PROD_HOST,
+//         port: process.env.DB_PROD_PORT,
+//         multipleStatements: true
+//     });
+   const con = mysql.createConnection(process.env.DATABASE_URL)
 //    con.connect();
     const testPassword = process.env.TEST_PASSWORD
     module.exports = {con, host, testPassword};
